4c90da253a9adad2a3ce6df4d6aed9f9ac95bd1c
lordsiinbal <79137266+lordsiinbal@users.noreply.github.com>
2021-07-16 17:37:32 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      'sm': {'min': '640px', 'max': '767px'},
+      'md': {'min': '768px', 'max': '1023px'},
+      'lg': {'min': '1024px', 'max': '1279px'},
+      'xl': {'min': '1280px', 'max': '1438px'},
+      '2xl': {'min': '1439px'},
+    },
     minWidth:{
       '1':'22.5rem'
     },

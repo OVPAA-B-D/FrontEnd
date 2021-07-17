7bb0b4eb66f06fe1d58fd90b39178d82bfa2788d
lordsiinbal <79137266+lordsiinbal@users.noreply.github.com>
2021-07-17 09:20:34 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/login.vue'
 import ProgramLevel from '../views/programLevel.vue'
 import Home_admin from '../views/Home_admin.vue'
 import Home_taskforce from '../views/Home_taskforce.vue'
 import Home_accreditor from '../views/Home_accreditor.vue'
+import Dashboard from '../views/dashboard_admin.vue'
 import Program_area from '../views/program_area.vue'
 import Program_parameter from '../views/program_parameter.vue'
 import Public_folder from '../views/public_folder.vue'
+import Office_admin from '../views/office_admin.vue'
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Login
   },
   {
     path: '/program_level',
@@ -24,6 +26,11 @@ const routes = [
     component: Program_area
   },
   {
+    path: '/office_admin',
+    name: 'Office Admin',
+    component: Office_admin
+  },
+  {
     path: '/home_admin',
     name: 'Home Admin',
     component: Home_admin
@@ -33,7 +40,11 @@ const routes = [
     name: 'Home Taskforce',
     component: Home_taskforce
   },
-
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard
+  },
 
   {
     path: '/program_parameter',

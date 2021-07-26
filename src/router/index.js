69cf09411a969cef37448057abac974c4379ed6e
lizpen <79520632+lizpen@users.noreply.github.com>
2021-07-26 16:02:03 +0800

    Update router

--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,15 @@ import Home_taskforce from '../views/Home_taskforce.vue'
 import Home_accreditor from '../views/Home_accreditor.vue'
 import Dashboard from '../views/dashboard_admin.vue'
 import Program_area from '../views/program_area.vue'
+import Program_area_taskforce from '../views/program_area_taskforce.vue'
 import Program_parameter from '../views/program_parameter.vue'
+import Program_parameter_taskforce from '../views/program_parameter_taskforce.vue'
 import Public_folder_admin from '../views/public_folder_admin.vue'
+import Public_folder_taskforce from '../views/public_folder_taskforce.vue'
+import Taskforce_members from '../views/taskforce_members.vue'
 import Office_admin from '../views/office_admin.vue'
+
+
 const routes = [
   {
     path: '/',
@@ -19,6 +25,11 @@ const routes = [
     path: '/program_level',
     name: 'Program Level',
     component: ProgramLevel
+  },
+  {
+    path: '/program_area_taskforce',
+    name: 'Program Area Taskforce',
+    component: Program_area_taskforce
   },
   {
     path: '/program_area',
@@ -51,7 +62,11 @@ const routes = [
     name: 'Program parameter',
     component: Program_parameter
   },
-
+  {
+    path: '/program_parameter_taskforce',
+    name: 'Program Parameter Taskforce',
+    component: Program_parameter_taskforce
+  },
   {
     path: '/public_folder',
     name: 'Public folder',
@@ -59,9 +74,21 @@ const routes = [
   },
 
   {
+    path: '/public_folder_taskforce',
+    name: 'Public folder',
+    component: Public_folder_taskforce
+  },
+
+  {
     path: '/home_accreditor',
     name: 'Home accreditor',
     component: Home_accreditor
+  },
+
+  {
+    path: '/taskforce_members',
+    name: 'Taskforce Members',
+    component: Taskforce_members
   },
 
   {
